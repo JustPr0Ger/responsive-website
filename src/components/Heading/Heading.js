f6d616ce69df49d73713356150351afcbc499662
Oleg <gki.proger@gmail.com>
2021-07-16 14:39:24 +0300

    change text

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeadingStyles';
 
 const Heading = (props) => (
-  <div>
-    Heading
-  </div>
+    <Section row nopadding>
+        <LeftSection>
+            <SectionTitle main center>
+                Hello,<br /> I'm 
+                Oleg Kutas
+            </SectionTitle>
+            <SectionText>
+                System Administrator and Front-End Developer who loves to code and learn new things each day.
+            </SectionText>
+        </LeftSection>
+    </Section>
 );
 
 export default Heading;
